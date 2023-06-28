621125ff4e1d7a6c792fa929d051954e39e4b57b
Vitória <129802405+VdeVitorinha@users.noreply.github.com>
2023-06-28 19:53:00 -0300

    Teste Critico 2 BaralhoTest.java

--- src/test/java/entidades/BaralhoTest.java
+++ src/test/java/entidades/BaralhoTest.java
@@ -33,8 +33,8 @@ public class BaralhoTest {
         assertEquals(cartasAntes.size(), cartasDepois.size());
         for (int i = 0; i < cartasAntes.size(); i++) {
             
-            Carta cartaAntes = cartasAntes.get(i);
-            //Carta cartaAntes = null;
+            //Carta cartaAntes = cartasAntes.get(i);
+            Carta cartaAntes = null;
             Carta cartaDepois = cartasDepois.get(i);
             
             assertEquals(cartaAntes.getNaipe(), cartaDepois.getNaipe());
